6451371a5f132793b30994f1e0f3b500e1e21c65
reddieabhi <91117253+reddieabhi@users.noreply.github.com>
2023-06-28 16:16:46 +0530

    Update DP 1 Frog jumps.md

--- DP 1 Frog jumps.m
+++ DP 1 Frog jumps.m
@@ -1,3 +1,7 @@
+## problem link: https://www.codingninjas.com/studio/problems/count-ways-to-reach-nth-stairs_798650?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
+
+
+
 def frogJump(n: int, heights) :
     ans = [10**9]*n
     ans[n-1] = 0
